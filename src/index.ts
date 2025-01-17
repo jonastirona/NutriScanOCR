1a1dc45406cffd55ca2fbe151aba11163cdefe6c
jonastirona <jonastirona1@gmail.com>
2025-01-16 20:31:40 -0500

    feat: switch deployment from CloudFormation to Serverless Framework

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import app from './app';
+import serverless from 'serverless-http';
+
+const handler = serverless(app);
+
+export const lambdaHandler = async (
+  event: APIGatewayProxyEvent,
+  context: any
+): Promise<any> => {
+  return await handler(event, context);
+};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+};
